870848dcfa172fc36dbd22337431973c8065ac90
whitebsm93 <whitebsm93@naver.com>
2022-08-03 21:20:52 +0900

    필요없는거 날리기

--- app.py
+++ app.py
@@ -23,63 +23,32 @@ SECRET_KEY = 'SPARTA'
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
-
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
         return render_template('index.html')
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-@app.route("/search", methods=["POST"])
-def post_search():
-    product_receive = request.form["product_give"]
-    results = search_keyword(product_receive)
-    return jsonify({'search_results': results})
-
-@app.route("/all_products", methods=["GET"])
-def get_all_products():
-    all_products_list = list(db.posting.find({},{'_id':False}))
-    return jsonify({'all_products' : all_products_list})
-
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
-
-
-@app.route('/user/<username>')
-def user(username):
-    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
-
-        user_info = db.users.find_one({"username": username}, {"_id": False})
-        return render_template('user.html', user_info=user_info, status=status)
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
-
 
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
     # 로그인
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
-
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     result = db.users.find_one({'username': username_receive, 'password': pw_hash})
-
     if result is not None:
         payload = {
          'id': username_receive,
          'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
-
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
     else:
@@ -94,15 +63,27 @@ def sign_up():
         "username": username_receive,                               # 아이디
         "password": password_hash,                                  # 비밀번호
         "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
+
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
+
+@app.route("/search", methods=["POST"])
+def post_search():
+    product_receive = request.form["product_give"]
+    results = search_keyword(product_receive)
+    return jsonify({'search_results': results})
+
+@app.route("/all_products", methods=["GET"])
+def get_all_products():
+    all_products_list = list(db.posting.find({},{'_id':False}))
+    return jsonify({'all_products' : all_products_list})
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
