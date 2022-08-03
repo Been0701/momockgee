d56a60accc49a9f530906d3856bf52a517c85bb0
kky7 <kykim708@gmail.com>
2022-08-03 21:43:03 +0900

    글쓰기업로드

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import hashlib
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
 from search import search_keyword
+import os.path
+from post_id import create_post_id
 
 
 app = Flask(__name__)
@@ -15,6 +17,10 @@ db = client.momockgee
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 SECRET_KEY = 'SPARTA'
+
+upload_forder = './upload'
+if not os.path.exists(upload_forder):
+    os.makedirs(upload_forder)
 
 # @app.route('/')
 # def home():
@@ -101,6 +107,59 @@ def comment_post():
     }
     db.comment.insert_one(doc)
     return jsonify({'msg': '등록 완료!'})
+
+@app.route('/posts')
+def post_page():
+   return render_template('write.html')
+
+
+@app.route('/posts', methods=['POST'])
+def post():
+    global cur_max_postid
+    ##### file
+    f = request.files.get('file')
+    file_dir = upload_forder + '/' + f.filename
+    f.save(file_dir)
+    post_img = '.' + file_dir
+    print(post_img)
+
+    ##### create post id
+    # post_list = list(db.Posting.find({},{'_id':False}))
+    # post_id_list = []
+    # for post_dic in post_list:
+    #     post_id_list.append(post_dic['post_id'])
+    #
+    # # print(post_id_list)
+    # post_id = random.randint(1, 1e7)
+    # while post_id in post_id_list:
+    #     # print('in while')
+    #     post_id = random.randint(1, 1e7)
+
+    post_id = create_post_id()
+    print(post_id)
+
+    ##### 그 외 front에서 받아 올 것
+    post_store = {'CU': 1, 'GS': 0, 'seven': 0, 'ministop': 0, 'emart25': 0}
+    post_content = '너무 맛있고 너무 맛있어요'
+    post_star = 3
+    post_product = '계란과자'
+
+    ## post_product 문자 모든 공백 제거
+    # post_product = post_product.replace(" ", "")
+
+    ##### db 저장
+    doc = {
+        'post_store': post_store,  # dict
+        'post_img': post_img,  # string
+        'post_content': post_content,  # string
+        'post_star': post_star,  # int
+        'post_product': post_product,  # string
+        'post_id': post_id,  # int
+    }
+
+    db.posting.insert_one(doc)
+
+    return jsonify({'msg': '저장완료'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
