d2af2f4267c85192593bc51bc11c218cd2db8e85
whitebsm93 <whitebsm93@naver.com>
2022-08-03 21:32:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -89,25 +89,18 @@ def comment_get():
     comments_list = list(db.comment.find({},{'_id':False}))
     return jsonify({'msg':'GET 연결 완료!'})
 
-
-
 @app.route("/comment", methods=["POST"])
 def comment_post():
     comment_receive = request.form['comment_give']
     comment_list = list(db.comment.find({}, {'_id': False}))
     count = len(comment_list) + 1
-
     doc = {
         # 'num': count,
         'num': 0,
         'comment': comment_receive
     }
-
     db.comment.insert_one(doc)
     return jsonify({'msg': '등록 완료!'})
-
-
-
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
