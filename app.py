37810d29de90a67326479c857da6c3f60623827c
whitebsm93 <whitebsm93@naver.com>
2022-08-04 16:22:14 +0900

    쓸모없는거 지우기

--- app.py
+++ app.py
@@ -22,53 +22,6 @@ upload_forder = './static/upload'
 if not os.path.exists(upload_forder):
     os.makedirs(upload_forder)
 
-# @app.route('/')
-# def home():
-#     msg = request.args.get("msg")
-#     return render_template('login.html', msg=msg)
-#
-#
-# @app.route('/login')
-# def login():
-#     return render_template('index.html')
-#
-#
-# @app.route('/sign_in', methods=['POST'])
-# def sign_in():
-#     # 로그인
-#     username_receive = request.form['username_give']
-#     password_receive = request.form['password_give']
-#     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-#     result = db.users.find_one({'username': username_receive, 'password': pw_hash})
-#     if result is not None:
-#         payload = {
-#          'id': username_receive,
-#          'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
-#         }
-#         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-#         return jsonify({'result': 'success', 'token': token})
-#     # 찾지 못하면
-#     else:
-#         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
-#
-# @app.route('/sign_up/save', methods=['POST'])
-# def sign_up():
-#     username_receive = request.form['username_give']
-#     password_receive = request.form['password_give']
-#     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-#     doc = {
-#         "username": username_receive,                               # 아이디
-#         "password": password_hash,                                  # 비밀번호
-#         "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-#     }
-#     db.users.insert_one(doc)
-#     return jsonify({'result': 'success'})
-#
-# @app.route('/sign_up/check_dup', methods=['POST'])
-# def check_dup():
-#     username_receive = request.form['username_give']
-#     exists = bool(db.users.find_one({"username": username_receive}))
-#     return jsonify({'result': 'success', 'exists': exists})
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
@@ -185,4 +138,4 @@ def post():
     return jsonify({'msg': '저장완료'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5003, debug=True)
+   app.run('0.0.0.0', port=5002, debug=True)
